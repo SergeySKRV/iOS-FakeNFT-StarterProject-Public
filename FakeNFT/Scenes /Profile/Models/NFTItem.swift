88337c2253601b3faac70ae3b6902ceafca3168f
SergeySKRV <ap0llyon@yandex.ru>
2025-09-13 21:43:52 +0300

    no message

---
+++ FakeNFT/Scenes /Profile/Models/NFTItem.swift
@@ -0,0 +1,20 @@
+//
+//  NFTItem.swift
+//  FakeNFT
+//
+//  Created by Сергей Скориков on 07.09.2025.
+//
+
+import Foundation
+
+// MARK: - NFTItem
+struct NFTItem {
+    
+    // MARK: - Properties
+    let id: String
+    let name: String
+    let rating: Int
+    let author: String
+    let price: String
+    let imageUrl: URL
+}
