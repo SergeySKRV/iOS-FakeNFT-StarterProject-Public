8b001f1014a907f3bae4b57e0cd230fda171f1f7
Михаил Волков <mike.volkov@mail.ru>
2025-09-21 15:34:06 +0300

    AppDelegate+SceneDelegate fix

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -1,6 +1,5 @@
-import Foundation
+import UIKit
 
-<<<<<<< HEAD
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,19 +17,5 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         options _: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-=======
-// MARK: - App Constants
-enum AppConstants {
-
-    // MARK: - User Defaults Keys
-    enum UserDefaultsKeys {
-        static let hasSeenOnboarding = "hasSeenOnboarding"
-    }
-
-    // MARK: - Onboarding
-    enum Onboarding {
-        static let autoScrollInterval: TimeInterval = 5.0
-        static let slideCount = 3
->>>>>>> caffbd545bc9f65f8285d216a2fea4663b2fd6df
     }
 }
