1a5bb308edc527025d76da3752d1b6914b9ae9fd
Михаил Волков <mike.volkov@mail.ru>
2025-09-21 19:13:22 +0300

    NFTDetails fix

---
+++ FakeNFT/Scenes /NftDetails/Cell/NftDetailCellModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// Модель ячейки для экрана деталей NFT.
+///
+/// Используется в `UITableView` или `UICollectionView`
+/// для отображения изображения NFT.
+struct NftDetailCellModel {
+    /// Ссылка на изображение NFT.
+    let url: URL
+}
