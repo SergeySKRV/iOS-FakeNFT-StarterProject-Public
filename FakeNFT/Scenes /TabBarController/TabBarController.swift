da151a5d2f14a2ded0f57a75a0e0faabd0594bc2
GeorgeModebadze <g.modebadze@kodington.ru>
2025-09-23 03:24:45 +0700

    рефактор таббара

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,100 +1,106 @@
 import UIKit
 
+// MARK: - TabBarController
 final class TabBarController: UITabBarController {
-    
-    var servicesAssembly: ServicesAssembly!
-    
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(resource: .catalogIcon),
-        tag: 0
-    )
-    
-    private let cartTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.cart", comment: ""),
-        image: UIImage(resource: .cartIcon),
-        tag: 1
-    )
-    
-    private let statisticsTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.statistics", comment: ""),
-        image: UIImage(resource: .statisticsIcon),
-        tag: 2
-    )
-    
-    private let profileTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.profile", comment: ""),
-        image: UIImage(resource: .profileIcon),
-        tag: 3
-    )
-    
+    private let servicesAssembly: ServicesAssembly
+    private var presenter: ProfilePresenterProtocol!
+
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        self.servicesAssembly = ServicesAssembly(
+            networkClient: DefaultNetworkClient(),
+            nftStorage: NftStorageImpl()
+        )
+        super.init(coder: coder)
+    }
+
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTabBarAppearance()
-        
-        
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
+        setupUI()
+
+        if viewControllers?.isEmpty ?? true {
+            setupViewControllers()
+        }
+    }
+
+    // MARK: - Private Methods
+    private func setupViewControllers() {
+        // Catalog зависимости
+        let networkClient = DefaultNetworkClient()
+        let catalogStorage = CatalogStorage()
+        let catalogService = CatalogService(
+            networkClient: networkClient,
+            catalogStorage: catalogStorage
         )
-        catalogController.tabBarItem = catalogTabBarItem
-        
-        let profileController = createPlaceholderController(title: "Профиль")
-        profileController.tabBarItem = profileTabBarItem
-        
+        let sortStorage = SortStorage()
+        let catalogPresenter = CatalogPresenter(
+            catalogService: catalogService,
+            sortStorage: sortStorage
+        )
+
+        // Профиль
+        let profileController = ProfileViewController(servicesAssembly: servicesAssembly)
+        let profileNav = UINavigationController(rootViewController: profileController)
+        profileNav.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.profile", comment: ""),
+            image: UIImage(resource: .profileIcon),
+            tag: 0
+        )
+
+        // Каталог
+        let catalogController = CatalogViewController(presenter: catalogPresenter)
+        let catalogNav = UINavigationController(rootViewController: catalogController)
+        catalogNav.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.catalog", comment: ""),
+            image: UIImage(resource: .catalogIcon),
+            tag: 1
+        )
+
+        // Корзина
         let cartService = CartService()
         let cartController = CartViewController()
-        let cartPresenter = CartPresenter(view: cartController, cartService: cartService)
+        let cartPresenter = CartPresenter(
+            view: cartController,
+            cartService: cartService
+        )
         cartController.presenter = cartPresenter
-        let cartNavigationController = UINavigationController(rootViewController: cartController)
-        cartNavigationController.tabBarItem = cartTabBarItem
-        
-        let statisticsController = createPlaceholderController(title: "Статистика")
-        statisticsController.tabBarItem = statisticsTabBarItem
-        
-        
-        viewControllers = [
-            profileController,
-            catalogController,
-            cartNavigationController,
-            statisticsController
-        ]
-        
-        view.backgroundColor = .systemBackground
+        let cartNav = UINavigationController(rootViewController: cartController)
+        cartNav.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.cart", comment: ""),
+            image: UIImage(resource: .cartIcon),
+            tag: 2
+        )
+
+        // Статистика
+        let statisticsController = StatisticsViewController()
+        let statisticsNav = UINavigationController(rootViewController: statisticsController)
+        statisticsNav.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.statistics", comment: ""),
+            image: UIImage(resource: .statisticsIcon),
+            tag: 3
+        )
+
+        // Устанавливаем контроллеры*/
+        viewControllers = [profileNav, catalogNav, cartNav, statisticsNav]
     }
-    
-    private func createPlaceholderController(title: String) -> UIViewController {
-        let controller = UIViewController()
-        controller.view.backgroundColor = .systemBackground
-        
-        let label = UILabel()
-        label.text = "\(title) в разработке"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        controller.view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: controller.view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: controller.view.centerYAnchor)
-        ])
-        
-        return controller
-    }
-    
-    private func setupTabBarAppearance() {
-        let appearance = UITabBarAppearance()
-        
-        appearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "ypBlack")
-        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
-            .foregroundColor: UIColor(named: "ypBlack") ?? .black
-        ]
-        
-        appearance.stackedLayoutAppearance.selected.iconColor = .yaBlueUniversal
-        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
-            .foregroundColor: UIColor.yaBlueUniversal
-        ]
-        
-        appearance.backgroundColor = .background
-        
-        UITabBar.appearance().standardAppearance = appearance
+
+    private func setupUI() {
+        view.backgroundColor = .yaSecondary
+
+        // Активный цвет (иконка + текст)
+        tabBar.tintColor = .yaBlueUniversal
+
+        // Неактивный цвет
+        tabBar.unselectedItemTintColor = .yaPrimary
+
+        // Цвет фона таббара
+        tabBar.backgroundColor = .yaSecondary
+        tabBar.isTranslucent = false
     }
 }
