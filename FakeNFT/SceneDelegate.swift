84d4805805c992848e8e36013deef0e64e8d4680
Alikagn <alikagn@mail.ru>
2025-09-23 22:23:53 +0300

    Добавлены апп делегат и сцена делегат

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -1,37 +1,32 @@
-import UIKit
+
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
-    var window: UIWindow?
 
+    var window: UIWindow?
     let servicesAssembly = ServicesAssembly(
         networkClient: DefaultNetworkClient(),
         nftStorage: NftStorageImpl()
     )
-    
-    func scene(_ scene : UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        guard let windowScene = ( scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        
-        let tabBarController = TabBarController()
-        window.rootViewController = tabBarController
-        self.window = window
-        window.makeKeyAndVisible()
-        
-    }
-    /*
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
         window = UIWindow(windowScene: windowScene)
-        let tabBarController = TabBarController()
-        window?.rootViewController = tabBarController
+
+        let shouldShowOnboarding = !UserDefaults.standard.bool(forKey: AppConstants.UserDefaultsKeys.hasSeenOnboarding)
+
+        if shouldShowOnboarding {
+            let onboardingVC = OnboardingViewController()
+            window?.rootViewController = onboardingVC
+        } else {
+            let tabBarController = TabBarController(servicesAssembly: servicesAssembly)
+            window?.rootViewController = tabBarController
+        }
+
         window?.makeKeyAndVisible()
     }
-     */
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        ReviewManager.incrementLaunchCountAndAskIfNeeded()
+    }
 }
-
-
