1410ace70c74d83383b33a6906627264b093bd84
Alikagn <alikagn@mail.ru>
2025-09-23 22:28:43 +0300

    Сделан таббар и реквестконстант

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -2,6 +2,23 @@ import UIKit
 
 // MARK: - TabBarController
 final class TabBarController: UITabBarController {
+    private let servicesAssembly: ServicesAssembly
+    private var presenter: ProfilePresenterProtocol!
+
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        self.servicesAssembly = ServicesAssembly(
+            networkClient: DefaultNetworkClient(),
+            nftStorage: NftStorageImpl()
+        )
+        super.init(coder: coder)
+    }
+
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +45,7 @@ final class TabBarController: UITabBarController {
         )
 
         // Профиль
-        let profileController = ProfileViewController()
+        let profileController = ProfileViewController(servicesAssembly: servicesAssembly)
         let profileNav = UINavigationController(rootViewController: profileController)
         profileNav.tabBarItem = UITabBarItem(
             title: NSLocalizedString("Tab.profile", comment: ""),
@@ -46,7 +63,13 @@ final class TabBarController: UITabBarController {
         )
 
         // Корзина
-        let cartController = ShoppingCartViewController()
+        let cartService = CartService()
+        let cartController = CartViewController()
+        let cartPresenter = CartPresenter(
+            view: cartController,
+            cartService: cartService
+        )
+        cartController.presenter = cartPresenter
         let cartNav = UINavigationController(rootViewController: cartController)
         cartNav.tabBarItem = UITabBarItem(
             title: NSLocalizedString("Tab.cart", comment: ""),
@@ -55,7 +78,7 @@ final class TabBarController: UITabBarController {
         )
 
         // Статистика
-        let statisticsController = StatisticViewController()
+        let statisticsController = StatisticsViewController()
         let statisticsNav = UINavigationController(rootViewController: statisticsController)
         statisticsNav.tabBarItem = UITabBarItem(
             title: NSLocalizedString("Tab.statistics", comment: ""),
@@ -63,7 +86,7 @@ final class TabBarController: UITabBarController {
             tag: 3
         )
 
-        // Устанавливаем контроллеры
+        // Устанавливаем контроллеры*/
         viewControllers = [profileNav, catalogNav, cartNav, statisticsNav]
     }
 
