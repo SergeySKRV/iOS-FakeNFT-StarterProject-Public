8ec18ba263c028cfc6f3eed06f13ad43c225b9e1
Alikagn <alikagn@mail.ru>
2025-09-24 20:33:51 +0300

    мелкие правки

---
+++ FakeNFT/Scenes /Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Profile.swift
+//  FakeNFT
+//
+//  Created by Dmitry Batorevich on 30.08.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    // TODO: реализация профиля
+}
+
---
+++ FakeNFT/Scenes /Statistics/StatisticsView/StatisticsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StatisticsViewController.swift
+//  FakeNFT
+//
+//  Created by Dmitry Batorevich on 30.08.2025.
+//
+
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    // TODO: реализация статистики
+}
