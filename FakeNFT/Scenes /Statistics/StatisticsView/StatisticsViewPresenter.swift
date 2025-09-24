bb5ad1051148612cc4eda0c3d4a66a2fcb817f1b
Михаил Волков <mike.volkov@mail.ru>
2025-09-24 11:47:57 +0300

    Response decoding fix

--- FakeNFT/Scenes /Statistics/StatisticsView/StatisticsViewPresenter.swift
+++ FakeNFT/Scenes /Statistics/StatisticsView/StatisticsViewPresenter.swift
@@ -86,13 +86,13 @@ final class StatisticsViewPresenter {
     private func convertStoreToViewModel(_ store: [StatisticsUser]) -> [StatisticsProfileModel] {
         var result: [StatisticsProfileModel] = []
         guard let users = storage.getUsers() else {return []}
+    
         for user in users {
-            guard let name = user.name else { break }
-            guard let nfts = user.nfts,
-                  let avatar = user.avatar,
-                  let rating = user.rating,
-                  let description = user.description
-            else { continue }
+            let name = user.name ?? "John Doe"
+            let nfts = user.nfts ?? []
+            let avatar = user.avatar ?? ""
+            let rating = Double(user.rating ?? "0.0") ?? 0.0
+            let description = user.description ?? ""
             let vmUser = StatisticsProfileModel(avatarImage: avatar,
                                                 description: description,
                                                 name: name,
